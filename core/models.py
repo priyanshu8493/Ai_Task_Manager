714d17f9ccd8723c2991200d2df361e86fbe8193
priyanshu333 <priyanshughosh333@gmail.com>
2025-05-30 18:47:25 +0530

    added task models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+STATUS_CHOICE = (
+    ('todo'), ('To Do'),
+    ('started'), ('Started'),
+    ('completed'), ('Completed')
+)
+
+class tasks(models.Model):
+    title = models.CharField(max_length = 100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_add_now = True)
+    status = models.CharField(max_length=20, choices = STATUS_CHOICE, default = 'todo')
+    deadline = models.DateField()
+
+
+    def __str__(self):
+        return self.title
